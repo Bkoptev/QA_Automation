e4a4ba8395ff9a7b4669fceff2d1008dac49a6ce
bkoptev <hate1smiley@gmail.com>
2019-11-07 18:53:49 +0200

    lesson3

---
+++ Lesson3/src/com/qa/lesson3/DaysOfWeek.java
@@ -0,0 +1,13 @@
+package com.qa.lesson3;
+
+public enum DaysOfWeek {
+    MONDAY("Monday", 1),
+    TUESDAY("Tuesday", 1),
+    WEDNESDAY("Wednesday", 1),
+    THURSDAY("Thursday", 1),
+    FRIDAY("Friday", 1),
+    SATURDAY("Saturday", 1),
+    SUNDAY("Sunday", 1);
+
+
+}
---
+++ Lesson3/src/com/qa/lesson3/TestInterface.java
@@ -0,0 +1,8 @@
+package com.qa.lesson3;
+
+public interface TestInterface {
+
+    void printPhrase();
+    void returnDayNameByNum(int numOfDay);
+    void returnDayNumByName(String name);
+}
