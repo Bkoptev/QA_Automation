52f398dd5703b0bdeadcb5c3b0fc187ec9d0bd0c
bkoptev <hate1smiley@gmail.com>
2019-11-11 18:56:25 +0200

    Restore3

---
+++ Lesson3/src/com/qa/lesson3/DaysOfWeek.java
@@ -0,0 +1,44 @@
+package com.qa.lesson3;
+
+public enum DaysOfWeek {
+    MONDAY("Monday", 1),
+    TUESDAY("Tuesday", 2),
+    WEDNESDAY("Wednesday", 3),
+    THURSDAY("Thursday", 4),
+    FRIDAY("Friday", 5),
+    SATURDAY("Saturday", 6),
+    SUNDAY("Sunday", 7);
+
+    private String value;
+    private int number;
+
+    /**
+     * Constructor
+     *
+     * @param name
+     * @param number
+     */
+    DaysOfWeek(String name, int number) {
+        this.value = name;
+        this.number = number;
+    }
+
+    /**
+     * Getter of day
+     *
+     * @return string with day
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Getter of number
+     *
+     * @return int with number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+}
